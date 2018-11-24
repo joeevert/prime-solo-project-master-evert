3d528436fd0d0ce038f4e83f4a42aff845427b7e
Joe Evert <joeevert@gmail.com>
2018-11-23 18:47:28 -0600

    imported mui to map view

--- src/components/MapView/MapContainer.js
+++ src/components/MapView/MapContainer.js
@@ -3,6 +3,57 @@ import { connect } from 'react-redux'
 import Map from './Map';
 import './MapContainer.css';
 
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  button: {
+      width: 300,
+      padding: 10,
+      backgroundColor: '#239956',
+      margin: theme.spacing.unit,
+  },
+  paper: {
+      width: 400,
+      height: 400,
+      borderRadius: 25,
+      margin: "auto",
+      marginTop: theme.spacing.unit * 10,
+      backgroundColor: '#67C28F',
+      border: '2px solid #01632C'
+  },
+  form: {
+      textAlign: "center",
+      padding: 15,
+      marginTop: theme.spacing.unit * 4,
+  },
+  textField: {
+      width: 300,
+      borderRadius: 5,
+      margin: theme.spacing.unit,
+      backgroundColor: '#fff'
+  },
+  header: {
+    color: '#fff', 
+    fontWeight: 'bold', 
+    backgroundColor: '#01632C', 
+    margin: '0', 
+    padding: '10px',
+    borderRadius: '22px 22px 0px 0px',
+  },
+  formControl: {
+      width: 300,
+      borderRadius: 5,
+      margin: theme.spacing.unit,
+      backgroundColor: '#fff'
+  },
+})
+
+
 class MapContainer extends Component {
 
   state = {
@@ -15,6 +66,7 @@ class MapContainer extends Component {
   }
   
   render() {
+    const { classes } = this.props;
     return (
       <div>
         <section style={{display: 'flex'}}>
@@ -42,4 +94,4 @@ class MapContainer extends Component {
 
 const mapStateToProps = reduxState => ({ reduxState });
 
-export default connect(mapStateToProps)(MapContainer);
+export default connect(mapStateToProps)(withStyles(styles)(MapContainer));
